4ac84b6a582486644e2f064f1cdfb9f3b77a110c
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-03-19 11:57:47 +0100

    render and redirect_to examples

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -3,4 +3,14 @@ class TestController < ApplicationController
     #odwołanie do pliku index.html.erb
     @testowa = "witam w kursie ruby on rails"
   end
+
+  def test
+    render('witaj')
+
+  end
+
+  def kurs
+    redirect_to('http://appchance.com/')
+
+  end
 end
