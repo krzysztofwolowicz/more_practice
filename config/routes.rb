ff212942a920d8974dde8d8def87352573fe6c87
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-03-19 11:43:26 +0100

    simple routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'test/index'
+  root 'test#index'
+  match ':controller(/:action(/:id))', :via => [:get, :post]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
