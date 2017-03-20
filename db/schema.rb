b6810c76c1744b8a84e828c3d15028a5559e0c00
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-03-20 09:05:56 +0100

    creating a model Kategorie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170319161513) do
+ActiveRecord::Schema.define(version: 20170320075752) do
+
+  create_table "kategories", force: :cascade do |t|
+    t.string   "nazwa"
+    t.integer  "pozycja"
+    t.boolean  "widoczna",   default: true
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "uzytkowniks", force: :cascade do |t|
     t.string   "imie",            limit: 20
