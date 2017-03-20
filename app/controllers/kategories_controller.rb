4ccf8a1be45e47a2ccdb1caa2dbfa5ce3ee6c466
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-03-20 11:02:26 +0100

    creating kategorie

--- app/controllers/kategories_controller.rb
+++ app/controllers/kategories_controller.rb
@@ -1,11 +1,32 @@
 class KategoriesController < ApplicationController
+
   def index
+
+    @kategorie = Kategorie.sortuj
+
   end
 
   def pokaz
   end
 
   def nowa
+# @kategoria to nazwa formularza a nie zmiennej
+    @kategoria = Kategorie.new({:nazwa => 'Nazwa'})
+    @licznik = Kategorie.count + 1
+
+  end
+
+  def utworz
+
+    @kategorie = Kategorie.new(kategorie_parametry)
+
+    if @kategorie.save
+      redirect_to(:action=>'index')
+    else
+      @licznik = Kategorie.count + 1
+      render('nowa')
+    end
+
   end
 
   def edycja
@@ -13,4 +34,11 @@ class KategoriesController < ApplicationController
 
   def usun
   end
+
+  def kategorie_parametry
+
+    params.require(:kategoria).permit(:nazwa, :pozycja, :widoczna)
+
+  end
+
 end
